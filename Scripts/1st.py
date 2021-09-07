9c4751471322bdc8835c11224c912179b43ed720
Yusuf Ansari <35006154+myusufuc@users.noreply.github.com>
2021-09-07 15:53:23 +0530

    Update 1st.py

--- Scripts/1st.py
+++ Scripts/1st.py
@@ -1,6 +1,6 @@
 # What are the current active cities in India which are scheduling Meetup Events?
 
-
+# To Start pyspark shell
 # ./pyspark --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2
 
 # Import Required Libraries
@@ -8,11 +8,13 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col, to_json, struct
 from pyspark.sql.types import *
 
+# Created spark session
 spark = SparkSession \
     .builder \
     .appName("Meetup") \
     .getOrCreate()
 
+# Created kafka consumer using spark readStream
 raw_df = spark \
     .readStream \
     .format("kafka") \
@@ -22,6 +24,7 @@ raw_df = spark \
     .load() \
     .selectExpr("CAST(value AS STRING)")
 
+# Created Schema for Structured Streaming
 schema = StructType(
     [
         StructField("venue", StructType([
@@ -65,13 +68,23 @@ schema = StructType(
     ]
 )
 
+# Applied schema on data
 schema_df = raw_df.select(from_json(raw_df.value, schema).alias("data"))
+
+# Filterd data by country code(US)
 us_df = schema_df.filter(col("data.group.group_country") == 'us')
+
+# Selecting country code column
 city_df = us_df.select(col("data.group.group_city").alias("city"))
+
+# Creating Spark SQL Table In Memory(RAM)
 sql = city_df.writeStream.outputMode("append").format('memory').queryName('this_query').start()
+
+# Runing Spark SQL quries on the table
 output = spark.sql("select city, count(*) as city_count from this_query group by city order by city_count desc")
 
+# Converted the table records to json and changed table the name to "values"
 output_df = (output.select(to_json(struct(col("*"))).alias("value")))
 
+# Sending the data to kafka brocker
 output_df.write.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("topic", "output").save()
-
